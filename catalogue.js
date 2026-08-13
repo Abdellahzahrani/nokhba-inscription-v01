0c2dda5e0025dd73786eb2bc9c3a94694c685efa
Abdellahzahrani <78935628+Abdellahzahrani@users.noreply.github.com>
2026-08-13 18:27:11 +0200

    Update catalogue.js

--- catalogue.js
+++ catalogue.js
@@ -1,10 +1,10 @@
 window.NOKHBA_CATALOG = {
   "Primaire": ["Français", "Mathématiques", "Arabe"],
-  "1AC": ["Mathématiques", "Français", "Anglais", "Physique-Chimie"],
+  "1AC": ["Mathématiques", "Français", "Anglais", "Physique-Chimie","SVT"],
   "2AC": ["Mathématiques", "Français", "Anglais", "Physique-Chimie", "SVT"],
-  "3AC": ["Mathématiques", "Français", "Anglais", "Physique-Chimie", "SVT"],
+  "3AC": ["Mathématiques", "Français", "Anglais", "Physique-Chimie", "SVT","Histoire-GEO","ARAB"],
   "Tronc Commun": ["Mathématiques", "Français", "Anglais", "Physique-Chimie", "SVT"],
-  "1BAC scientifique": ["Mathématiques", "Physique-Chimie", "SVT", "Français", "Anglais", "Philosophie"],
-  "1BAC lettres": ["Français", "Anglais", "Arabe", "Philosophie", "Histoire-Géographie"],
+  "1BAC scientifique": ["Mathématiques", "Physique-Chimie", "SVT", "Français", "Arab", "Histoire-GEO", "Education islamique"],
+  "1BAC lettres": ["Français","Mathématiques"],
   "2BAC": ["Physique-Chimie", "Mathématiques", "SVT", "Anglais", "Philosophie"]
 };
